e22c178f6ec0550785f6b2d3c9e9923eca26d804
Allen Tran <fakeallentran@gmail.com>
2015-07-02 10:54:57 -0700

    logging and wrap in main

--- graph2vec/trainer.py
+++ graph2vec/trainer.py
@@ -2,6 +2,7 @@ __author__ = 'porky-chu'
 
 import random
 import os
+import logging
 
 import numpy as np
 
@@ -9,8 +10,8 @@ from node_vectors import NodeVectorModel
 import parser
 
 
-class Node2Vec(object):
-    def __init__(self, vector_dimensions, output_dir, graph_path=None):
+class Graph2Vec(object):
+    def __init__(self, vector_dimensions, output_dir):
 
         self.output_dir = output_dir
 
@@ -27,49 +28,66 @@ class Node2Vec(object):
         graph.save_mappings(self.output_dir)
 
         if load_mmap:
-            self.from_to_idxs = np.memmap(os.path.join(data_dir, 'from_to.mat'), mode='r')
-            self.inverse_degrees = np.memmap(os.path.join(data_dir, 'inverse_degrees.mat'), mode='r')
+            self.inverse_degrees = np.memmap(
+                os.path.join(data_dir, 'inverse_degrees.mat'),
+                mode='r',
+                dtype='float32'
+            )
+            self.from_to_idxs = np.memmap(
+                os.path.join(data_dir, 'from_to.mat'),
+                mode='r',
+                dtype='int32'
+            )
+            self.from_to_idxs = np.reshape(self.from_to_idxs, newshape=(self.inverse_degrees.shape[0], 2))
         else:
             from_to_idxs, inverse_degrees = graph.extend_graph(max_degree=2)
-            self.from_to_idxs = np.memmap(os.path.join(data_dir, 'from_to.mat'), mode='w+', shape=from_to_idxs.shape)
+            self.from_to_idxs = np.memmap(
+                os.path.join(data_dir, 'from_to.mat'),
+                mode='r+',
+                shape=from_to_idxs.shape,
+                dtype='int32'
+            )
             self.from_to_idxs[:] = from_to_idxs[:]
-            self.inverse_degrees = np.memmap(os.path.join(data_dir, 'inverse_degrees.mat'), mode='w+', shape=inverse_degrees.shape)
+            self.inverse_degrees = np.memmap(
+                os.path.join(data_dir, 'inverse_degrees.mat'),
+                mode='r+',
+                shape=inverse_degrees.shape,
+                dtype='float32'
+            )
             self.inverse_degrees[:] = inverse_degrees[:]
 
 
-    def fit(self, max_epochs=100, batch_size=1000, seed=1692, cost_tol=-np.inf):
+    def fit(self, max_epochs=100, batch_size=1000, seed=1692):
+
         self.model = NodeVectorModel(
             n_from=len(self.from_nodes),
             n_to=len(self.to_nodes),
             de=self.dimensions
         )
 
-        cost0 = np.inf
         random.seed(seed)
         shuffled_idxes = np.arange(self.from_to_idxs.shape[0])
         for epoch_idx in xrange(max_epochs):
 
             random.shuffle(shuffled_idxes)
 
-            cost1 = []
+            cost = []
             for obs_idx in xrange(0, len(self.inverse_degrees), batch_size):
-                cost1.append(self.model.train(self.from_to_idxs[shuffled_idxes[obs_idx:obs_idx + batch_size]],
+                cost.append(self.model.train(self.from_to_idxs[shuffled_idxes[obs_idx:obs_idx + batch_size]],
                                           self.inverse_degrees[shuffled_idxes[obs_idx:obs_idx + batch_size]]))
                 self.model.update_params(self.from_to_idxs[shuffled_idxes[obs_idx:obs_idx + batch_size]],
                                          self.inverse_degrees[shuffled_idxes[obs_idx:obs_idx + batch_size]])
-                # self.model.normalize(self.from_to_idxs[shuffled_idxes[obs_idx:obs_idx + batch_size]])
+                self.model.normalize(self.from_to_idxs[shuffled_idxes[obs_idx:obs_idx + batch_size]])
 
-            # print self.model.calculate_loss(self.from_to_idxs, self.inverse_degrees)
+            cost = np.mean(cost)
+            logging.info('After %s epochs, cost=%s' % (epoch_idx, cost ** 0.5))
 
-            cost1 = np.mean(cost1)
-            if np.abs(cost0 - cost1) < cost_tol:
-                return
-            cost0 = cost1
-            print cost1 ** 0.5
-
+def main():
+    node2vec = Graph2Vec(vector_dimensions=128, output_dir='data')
+    node2vec.parse_graph('data/edge.list', load_mmap=True)
+    node2vec.fit()
+    node2vec.model.save_to_file("data/case_embeddings.pkl")
 
 if __name__ == "__main__":
-    node2vec = Node2Vec(graph_path="data/edge.list", vector_dimensions=128, output_dir='data')
-    node2vec.parse_graph('data/edge.list', load_mmap=False)
-    node2vec.fit(cost_tol=1e-10)
-    node2vec.model.save_to_file("data/case_embeddings.pkl")
+    logging.basicConfig(level=logging.INFO)
+    main()
