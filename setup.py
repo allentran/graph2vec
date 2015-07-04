44e82d1624c663a42817db7025492467f7eb5bd1
Allen Tran <fakeallentran@gmail.com>
2015-07-04 09:04:51 -0700

    version to 0.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     name = 'graph2vec'
     setup(
         name=name,
-        version="0.0.1",
+        version="0.0.2",
         author='Allen Tran',
         author_email='realallentran@gmail.com',
         description='meaningful vector representations of nodes',
